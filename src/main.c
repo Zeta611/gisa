55d50560ce51ba3ada0901157d151582fed5b685
Jaeho Lee <jaeho.lee@snu.ac.kr>
2021-04-24 13:03:48 +0000

    :sparkles: Add argument parsing

--- src/main.c
+++ src/main.c
@@ -1,11 +1,26 @@
 #include "../build/src/parser.tab.h"
+#include <stdbool.h>
 #include <stdio.h>
 
 char *progname;
 int lineno = 1;
+bool fin = false;
+extern FILE *yyin;
 
 int main(int argc, char *argv[])
 {
+	progname = argv[0];
+
+	// Handle input file.
+	if (argc > 1) {
+		fin = true;
+		yyin = fopen(argv[1], "r");
+	}
+
 	yyparse();
+
+	if (fin) {
+		fclose(yyin);
+	}
 	return 0;
 }
