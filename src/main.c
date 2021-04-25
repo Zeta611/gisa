36fc5dbf6e9bf9ac6056f8d0df706181622d79ce
Jaeho Lee <jaeho.lee@snu.ac.kr>
2021-04-25 08:14:11 +0000

    :goal_net: Handle file errors

--- src/main.c
+++ src/main.c
@@ -1,26 +1,38 @@
 #include "parser.tab.h"
-#include <stdbool.h>
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 char *progname;
 int lineno = 1;
-bool fin = false;
 extern FILE *yyin;
 
 int main(int argc, char *argv[])
 {
 	progname = argv[0];
 
+	// Input file name
+	char *fin = NULL;
 	// Handle input file.
 	if (argc > 1) {
-		fin = true;
-		yyin = fopen(argv[1], "r");
+		fin = argv[1];
+		errno = 0;
+		if (!(yyin = fopen(argv[1], "r"))) {
+			fprintf(stderr, "%s: cannot access '%s': %s\n",
+				progname, fin, strerror(errno));
+			exit(EXIT_FAILURE);
+		}
 	}
 
 	yyparse();
 
 	if (fin) {
-		fclose(yyin);
+		if (fclose(yyin)) {
+			fprintf(stderr, "%s: fclose error '%s': %s\n", progname,
+				fin, strerror(errno));
+			exit(EXIT_FAILURE);
+		}
 	}
 	return 0;
 }
